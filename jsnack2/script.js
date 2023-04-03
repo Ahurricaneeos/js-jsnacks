173e427a747814a92937532c80cef1464301ba08
Ahurricaneeos <aurora_sala@hotmail.it>
2023-04-03 14:34:32 +0200

    second jsnack

---
+++ jsnack2/script.js
@@ -0,0 +1,8 @@
+let firstWord = prompt("Inserisci una parola:");
+let secondWord = prompt("Inserisci un'altra parola");
+
+if (firstWord > secondWord)
+    console.log(secondWord + " " + firstWord);
+else {(firstWord < secondWord)
+    console.log(firstWord + " " + secondWord);
+}
