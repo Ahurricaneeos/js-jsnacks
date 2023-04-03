3a04f4e5086384f51f689a3d56dba2d4bfa918f2
Ahurricaneeos <aurora_sala@hotmail.it>
2023-04-03 14:21:34 +0200

    first js snack

---
+++ jsnack1/script.js
@@ -0,0 +1,8 @@
+let firstNumber = prompt("inserisci un numero:");
+let secondNumber = prompt("inserisci un altro numero:");
+
+if (firstNumber > secondNumber)
+    console.log(firstNumber);
+else {(firstNumber < secondNumber)
+    console.log(secondNumber);
+}
